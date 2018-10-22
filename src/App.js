e8be684c3ddc259d4935a620e19aea15f9e8decc
Im Khem <im.khem@gmail.com>
2018-10-21 18:07:00 -0700

    added dialog

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { withStyles } from '@material-ui/core/styles';
 
 import Button from '@material-ui/core/Button';
 
+import AddDialog from './components/AddDialog';
+
 const styles =  {
   root: {
     width: '100%',
@@ -15,23 +17,28 @@ const styles =  {
     backgroundColor: 'blue',
   },
   addBtn: {
-    height: 35,
-    width: 35,
+    height: '60%',
     color: 'white',
-    backgroundColor: 'Orange',
+    backgroundColor: 'Aqua',
     margin: 15,
+    fontSize: 25,
   },
 }
 
 class App extends Component {
 
-  state = { isOpen: false };
+  state = { isOpen: false, type: ['Tags', 'Categories'] };
 
   openDialog = () => {
     this.setState({ isOpen: true });
   }
 
+  handleClose = () => {
+    this.setState({ isOpen: false });
+  };
+
   render() {
+    const { isOpen } = this.state;
     const { classes } = this.props;
     return (
       <div className={classes.root}>
@@ -43,6 +50,14 @@ class App extends Component {
             +
           </Button>
         </header>
+        { isOpen ? 
+          <AddDialog
+            open={isOpen}
+            handleClose={this.handleClose}
+            type={this.state.type}
+          /> 
+          : null 
+        }
       </div>
     );
   }
---
+++ src/components/AddDialog/index.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+
+import DownShift from 'downshift';
+
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import TextField from '@material-ui/core/TextField';
+
+const styles = {
+  select: {
+    width: 150,
+  },
+  main: {
+    display: 'flex',
+  },
+  kdshift: {
+    paddingLeft: 20,
+  }
+}
+
+class AddDialog extends React.Component {
+  state = { title: '', type: '' }
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleSubmit = (event) => {
+    console.log({ event: event.target })
+    this.setState({ open: false });
+  }
+
+  render() {
+    const { open, type, classes } = this.props;
+    return (
+      <Dialog
+        open={open}
+        onClose={this.props.handleClose}
+        aria-labelledby="form-dialog-title"
+        fullWidth
+      >
+        <DialogTitle id="form-dialog-title">Create List</DialogTitle>
+        <DialogContent className={classes.main}>
+          <div className={classes.typeSelect}>
+            <Select
+              value={this.state.type}
+              onChange={this.handleChange}
+              className={classes.select}
+            >
+              { type ? 
+                type.map((item) => (
+                  <MenuItem 
+                    value={item}
+                    key={item}
+                  >
+                    {item}
+                  </MenuItem>
+                )) : null
+              }
+            </Select>
+          </div>
+
+          <div className={classes.kdshift}>
+            <DownShift>
+              {({ isOpen, toggleMenu, getInputProps }) => (
+                <div>
+                  <TextField />
+
+                  
+                </div>
+              )}
+            </DownShift>
+          </div>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.props.handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={this.handleSubmit} color="primary">
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
+  )};
+};
+
+AddDialog.propTypes = {
+  classes: PropTypes.object
+};
+
+export default withStyles(styles)(AddDialog);
